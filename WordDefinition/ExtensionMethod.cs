5484c4fb408696eea9dab56d894783e0af835853
(Charlie)YanzuoGe <42125596+geyanzuo@users.noreply.github.com>
2019-08-26 16:52:49 +1000

    Add files via upload

---
+++ WordDefinition/ExtensionMethod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordDefinition
+{
+    public static class StringExtension
+    {
+        public static string AlternateCase(this string original)
+        {
+            string reslut = string.Empty;
+
+            char[] chars = original.ToCharArray();
+            for (int i =0; i < chars.Length; i++)
+            {
+                if (i%2 == 0)
+                {
+                    reslut += chars[i].ToString().ToLower();
+                }
+                else
+                {
+                    reslut += chars[i].ToString().ToUpper();
+                }
+            }
+            return reslut;
+        }
+
+
+        public static string ShiftAmt(this string original, int shiftamt)
+        {
+            string result = string.Empty;
+            char[] chars = original.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == 'Z')
+                {
+                    result += chars[i] = 'A';
+                }
+
+                else if (chars[i] == 'z')
+                {
+                    result += chars[i] = 'a';
+                }
+                else
+                {
+                    result += chars[i] = (char)(((int)chars[i]) + shiftamt);
+                }
+            }
+
+            return result;
+        }
+
+    }
+
+    
+}
---
+++ WordDefinition/WordDefinition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace WordDefinition
+{
+    public class WordDefinition
+    {
+    
+
+        public string AlternatingCase(string input)
+        {
+            input = input.AlternateCase();
+            return input;
+        }
+
+        public string ShiftWord(string input, int shiftamt)
+        {
+            input = input.ShiftAmt(shiftamt);
+            return input;
+        }
+
+        public string MostCommonLetter(string input)
+        {
+            var charGroups = (from c in input
+                              group c by c into g
+                              select new
+                              {
+                                  c = g.Key,
+                                  count = g.Count(),
+                              }).OrderByDescending(c => c.count);
+            return input;
+
+            
+        }
+
+        public int AmtOfDifferentChars(string input)
+        {
+            int count = 0;
+            
+            foreach (char c in input)
+            {
+                if (input.Contains(c))
+                    count++;
+            }
+            return count;
+
+        }
+    }
+
+
+}
